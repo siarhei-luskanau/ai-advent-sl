a855788ea44046c21f05972d3d7104d8c0df4fd0
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2026-01-12 21:32:35 +0100

    Added empty Gradle project

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "ai-advent-sl"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
